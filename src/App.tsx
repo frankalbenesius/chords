1c01eddd31a46b1b0a0834effd23fed40727e1c0
Frank Albenesius <frankalbenesius@gmail.com>
2024-03-14 13:58:49 -0500

    Mock notation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="sections">
+        <KeySection />
+        <ProgressionSection />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+const SHARP = "♯";
+const FLAT = "♭";
+
+const KeySection = () => {
+  return (
+    <section className="section key">
+      <div className="content">
+        C<span className="modifier">{SHARP}</span>
+      </div>
+      <div className="label">key</div>
+    </section>
+  );
+};
+
+const ProgressionSection = () => {
+  return (
+    <section className="section progression">
+      <div className="content">I - iii - IV - vi</div>
+      <div className="label">progression</div>
+    </section>
+  );
+};
